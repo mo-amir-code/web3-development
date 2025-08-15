3dc329fdfad0b402009d79008656b1139e829077
Mo Amir <mo.amir.code@gmail.com>
2025-08-16 01:16:57 +0530

    temp data storage with api

--- solana/projects/cen_lst/src/index.ts
+++ solana/projects/cen_lst/src/index.ts
@@ -1,30 +1,35 @@
-import express from 'express';
-import { burnTokens, mintTokens, sendNativeTokens } from './mintTokens';
+import express from "express";
+import { burnTokens, mintTokens, sendNativeTokens } from "./mintTokens";
 
 const app = express();
+app.use(express.json());
 
+const data: any = [];
 
-app.post('/helius', async(req:any, res:any) => {
-    console.log(req.body)
+app.post("/helius", async (req: any, res: any) => {
+  console.log(req.body);
+  data.push(req.body);
 
+  // const fromAddress = req.body.fromAddress;
+  // const toAddress = req.body.toAddress;
+  // const amount = req.body.amount;
+  // const type = "received_native_sol";
 
-    // const fromAddress = req.body.fromAddress;
-    // const toAddress = req.body.toAddress;
-    // const amount = req.body.amount;
-    // const type = "received_native_sol";
+  // if (type === "received_native_sol") {
+  //     await mintTokens(fromAddress, toAddress, amount);
+  // } else {
+  //     // What could go wrong here?
+  //     await burnTokens(fromAddress, toAddress, amount);
+  //     await sendNativeTokens(fromAddress, toAddress, amount);
+  // }
 
+  res.send("Transaction successful");
+});
 
-    // if (type === "received_native_sol") {
-    //     await mintTokens(fromAddress, toAddress, amount);
-    // } else {
-    //     // What could go wrong here?
-    //     await burnTokens(fromAddress, toAddress, amount);
-    //     await sendNativeTokens(fromAddress, toAddress, amount);
-    // }
-
-    res.send('Transaction successful');
+app.get("/", (_req, res) => {
+  res.json(data);
 });
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  console.log("Server is running on port 3000");
 });
