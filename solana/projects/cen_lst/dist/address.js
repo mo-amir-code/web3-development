1aac16996732592dbfaeea1a77ac9958be0d64aa
Mo Amir <mo.amir.code@gmail.com>
2025-08-16 00:32:34 +0530

    node types added

--- solana/projects/cen_lst/dist/address.js
+++ solana/projects/cen_lst/dist/address.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.TOKEN_MINT_ADDRESS = exports.PUBLIC_KEY = exports.PRIVATE_KEY = void 0;
-exports.PRIVATE_KEY = "";
-exports.PUBLIC_KEY = "";
-exports.TOKEN_MINT_ADDRESS = "";
+exports.PRIVATE_KEY = "2gakfY6Jdc1p5qyhAvDjiYkU3JtLtvajpSJsiy8DJw2gdJzbUtL5pL6H7Uio7AMXQesz2ieH9oia3tYk7Z4ta7uk";
+exports.PUBLIC_KEY = "4ExCkT4h9rimmjoaaN8qp4AUzRkRChKPhJBx5stfeQAv";
+// MekYu token mint address of devnet
+exports.TOKEN_MINT_ADDRESS = "7U1hzMCuar5PjgGVGw58nswow5f4p9D4NTD1JUF2ch1P";
--- solana/projects/cen_lst/dist/index.js
+++ solana/projects/cen_lst/dist/index.js
@@ -12,7 +12,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-require('dotenv').config();
 const express_1 = __importDefault(require("express"));
 const mintTokens_1 = require("./mintTokens");
 const app = (0, express_1.default)();
