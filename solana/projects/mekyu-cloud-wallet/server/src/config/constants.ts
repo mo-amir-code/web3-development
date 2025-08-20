f77b2c0249262158dbd6fc43af72dfd84b9262f3
Mo Amir <mo.amir.code@gmail.com>
2025-08-20 23:01:30 +0530

    Few changes related to deployment

--- solana/projects/mekyu-cloud-wallet/server/src/config/constants.ts
+++ solana/projects/mekyu-cloud-wallet/server/src/config/constants.ts
@@ -2,25 +2,26 @@
 const APP_PORT = process.env.APP_PORT || 8080;
 const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN;
 const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
-const DB_URI = process.env.DB_URI!;
+// const DB_URI = process.env.DB_URI!;
 const ENVIRONMENT = process.env.ENVIRONMENT;
-const GOOGLE_APPLICATION_CREDENTIALS = process.env.GOOGLE_APPLICATION_CREDENTIALS;
+const GOOGLE_APPLICATION_CREDENTIALS =
+  process.env.GOOGLE_APPLICATION_CREDENTIALS;
 const ALCHEMY_KEY = process.env.ALCHEMY_KEY;
 
 const WHITELISTED_ORIGINS = [CLIENT_ORIGIN];
 
-const SOLANA_MAINNET = `https://solana-mainnet.g.alchemy.com/v2/${ALCHEMY_KEY}`
-const SOLANA_DEVNET = `https://solana-devnet.g.alchemy.com/v2/${ALCHEMY_KEY}`
+const SOLANA_MAINNET = `https://solana-mainnet.g.alchemy.com/v2/${ALCHEMY_KEY}`;
+const SOLANA_DEVNET = `https://solana-devnet.g.alchemy.com/v2/${ALCHEMY_KEY}`;
 // const SOLANA_DEVNET = `https://api.devnet.solana.com`
 
 export {
   APP_PORT,
   JWT_SECRET_KEY,
-  DB_URI,
+  // DB_URI,
   ENVIRONMENT,
   WHITELISTED_ORIGINS,
   GOOGLE_APPLICATION_CREDENTIALS,
   ALCHEMY_KEY,
   SOLANA_DEVNET,
-  SOLANA_MAINNET
+  SOLANA_MAINNET,
 };
